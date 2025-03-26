f90d7529573f23c62b630230fa8b6ec0ddf668c7
MDR <imdanya946@gmail.com>
2025-03-26 19:59:41 +0200

    change revies

--- app/[local]/components/reviewsContainer.tsx
+++ app/[local]/components/reviewsContainer.tsx
@@ -8,52 +8,98 @@ import CommentModal from "./commentModal";
 export default function ReviewsContainer() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const reviews = [
     {
-      email: "lgovody@ukr.net",
+      id: 1,
+      email: "ivanov123@ukr.net",
       rating: 4,
-      comment: "Прекрасний Сервіс. Рік вже дивлюсь Filmix Pro без проблем .",
+      comment:
+        "Один із найкращих сервісів! Порадили знайомі, але це просто топ — підписки на 50-70% дешевші. Рекомендую спробувати)",
     },
     {
-      username: "Катерина",
-      rating: 2,
-      comment: "Чудовий сервіс ,підтримка завжди на зв'язку 👍👍👍",
-    },
-    {
-      email: "onevardd@gmail.com",
+      id: 2,
+      username: "Марина",
       rating: 5,
       comment:
-        "Взял Netflix - восторг. Все работает как часы. Планирую еще GPT+ взять",
+        "Дуже задоволена підписками. Підтримка чудова, швидко відповідають і допомогли з підключенням однієї з них за кордоном. Безмежно дякую!!!",
     },
     {
-      username: "Катерина",
-      rating: 3,
-      comment: "Чудовий сервіс ,підтримка завжди на зв'язку 👍👍👍",
-    },
-    {
-      email: "onevardd@gmail.com",
+      id: 3,
+      email: "pavlo.d@gmail.com",
       rating: 5,
       comment:
-        "Взял Netflix - восторг. Все работает как часы. Планирую еще GPT+ взять",
+        "Доброзичливістю і готовністю вирішити всі складнощі в ситуації - я максимально вражений! Такої техпідтримки я ще не зустрічав. Дуже вдячний!",
     },
     {
-      username: "Катерина",
-      rating: 3,
-      comment: "Чудовий сервіс ,підтримка завжди на зв'язку 👍👍👍",
-    },
-    {
-      email: "onevardd@gmail.com",
+      id: 4,
+      username: "Олексій",
       rating: 5,
       comment:
-        "Взял Netflix - восторг. Все работает как часы. Планирую еще GPT+ взять",
+        "Пробував різні сервіси, вже декілька місяців поспіль користуюсь чатом GPT - незамінна річ. Дякую, що зробили це доступним для мене.",
     },
     {
-      username: "Катерина",
-      rating: 3,
-      comment: "Чудовий сервіс ,підтримка завжди на зв'язку 👍👍👍",
+      id: 5,
+      email: "anna.petrenko@mail.com",
+      rating: 5,
+      comment:
+        "Я балдію! Зекономила 300 грн і дивлюся собі той Нетфлікс реально цілими днями) Дякую вам 🤍",
+    },
+    {
+      id: 6,
+      username: "Віталій",
+      rating: 5,
+      comment:
+        "Велике спасибі за вашу роботу! Підписки дійсно коштують дешевше, все швидко і зручно. Користуюсь із задоволенням, все працює відмінно.",
+    },
+    {
+      id: 7,
+      email: "dmytro.levchuk@ukr.net",
+      rating: 5,
+      comment:
+        "Просто дякую за сервіс! Раніше я використовував подібний метод із друзями, але тут можна дуже непогано заощадити. Щиро дякую і бажаю успіхів!",
+    },
+    {
+      id: 8,
+      username: "Тетяна",
+      rating: 4,
+      comment:
+        "Сервіс, котрий заощаджує мені кошти. Зручно та ефективно. Чекаю на розширення сервісів, доступних для підписок.",
+    },
+    {
+      id: 9,
+      username: "Юлія",
+      rating: 5,
+      comment:
+        "Найкращі! Підтримка в Телеграмі — то окреме кохання! Задавала 1000 питань, на всі відповіли і допомогли ♥️",
+    },
+    {
+      id: 10,
+      username: "Сергій",
+      rating: 5,
+      comment:
+        "Навіть не уявляю, як обходився без вас раніше) Рекомендую всім!",
+    },
+    {
+      id: 11,
+      username: "Олена",
+      rating: 5,
+      comment:
+        "Ваш сервіс — це реально справжня знахідка! Доступні підписки, відмінна якість, і головне — значна економія ) Тепер всі мої друзі теж користуються!",
+    },
+    {
+      id: 12,
+      username: "Максим",
+      rating: 5,
+      comment:
+        "Я радив вас своїм друзям, які купували напряму Нетфлікс. Говорив, що так дешевше! Тепер всі разом користуємось! Все подобається!",
     },
   ];
-  const totalSlides = Math.ceil(reviews.length / 2);
+  const getItemsPerSlide = () => {
+    return isMobile ? 2 : 4;
+  };
+
+  const totalSlides = Math.ceil(reviews.length / getItemsPerSlide());
 
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev + 1) % totalSlides);
@@ -63,16 +109,11 @@ export default function ReviewsContainer() {
     setCurrentSlide((prev) => (prev - 1 + totalSlides) % totalSlides);
   };
 
-  const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
+      setIsMobile(window.innerWidth < 768);
     };
+
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => {
@@ -81,15 +122,7 @@ export default function ReviewsContainer() {
   }, []);
 
   useEffect(() => {
-    if (isModalOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
+    document.body.style.overflow = isModalOpen ? "hidden" : "auto";
   }, [isModalOpen]);
 
   return (
@@ -105,9 +138,7 @@ export default function ReviewsContainer() {
             </h2>
             <div className="flex items-center gap-4">
               <button
-                onClick={() => {
-                  setIsModalOpen(true);
-                }}
+                onClick={() => setIsModalOpen(true)}
                 className="px-6 py-2 rounded-full border border-b-indigo-600 text-indigo-600 hover:bg-indigo-600 hover:text-white transition-colors cursor-pointer"
               >
                 {useClientTranslation("add_comment")}
@@ -141,12 +172,15 @@ export default function ReviewsContainer() {
               {Array.from({ length: totalSlides }).map((_, slideIndex) => (
                 <div
                   key={slideIndex}
-                  className="w-full flex-shrink-0 grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-6 p-2"
+                  className="w-full flex-shrink-0 grid grid-cols-1 md:grid-cols-2 gap-6 p-2"
                 >
                   {reviews
-                    .slice(slideIndex * 2, slideIndex * 2 + (isMobile ? 2 : 4))
-                    .map((review, reviewIndex) => (
-                      <ReviewItem key={reviewIndex} {...review} />
+                    .slice(
+                      slideIndex * getItemsPerSlide(),
+                      (slideIndex + 1) * getItemsPerSlide()
+                    )
+                    .map((review) => (
+                      <ReviewItem key={review.id} {...review} />
                     ))}
                 </div>
               ))}
