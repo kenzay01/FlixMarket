96e1a73f36e190e827c38778a7f7f7e2716d664c
MDR <imdanya946@gmail.com>
2025-03-29 14:47:30 +0200

    clear code

--- components/languageRedirect.tsx
+++ components/languageRedirect.tsx
@@ -1,51 +1,3 @@
-// "use client";
-
-// import { useEffect } from "react";
-// type Locale = "en" | "ua" | "de";
-
-// const COUNTRY_TO_LOCALE: Record<string, Locale> = {
-//   UA: "ua",
-//   US: "en",
-//   GB: "en",
-//   DE: "de",
-// };
-
-// const LanguageRedirect = () => {
-//   useEffect(() => {
-//     if (typeof window !== "undefined") {
-//       const locale = window.location.pathname.split("/")[1] || "en";
-//       const storageLocale = localStorage.getItem("language");
-
-//       if (!storageLocale) {
-//         const detectLanguage = async () => {
-//           try {
-//             const response = await fetch("https://ipapi.co/json/");
-//             if (!response.ok) throw new Error("Геолокація не вдалася");
-
-//             const data = await response.json();
-//             const country = data.country_code;
-//             const language = COUNTRY_TO_LOCALE[country] || "en";
-
-//             localStorage.setItem("language", language);
-//             window.location.href = `/${language}`;
-//           } catch (error) {
-//             console.warn("Помилка геолокації:", error);
-//           }
-//         };
-
-//         detectLanguage();
-//       }
-//       if (storageLocale !== locale) {
-//         window.location.href = `/${localStorage.getItem("language")}`;
-//       }
-//     }
-//   }, []);
-
-//   return null;
-// };
-
-// export default LanguageRedirect;
-
 "use client";
 
 import { useEffect } from "react";
@@ -67,14 +19,12 @@ const LanguageRedirect = () => {
     const storedLocale = localStorage.getItem("language") as Locale | null;
 
     if (storedLocale) {
-      // Якщо у localStorage вже є мова, але вона відрізняється від поточної в URL
       if (storedLocale !== currentPathLocale) {
         window.location.href = `/${storedLocale}`;
       }
       return;
     }
 
-    // Якщо в localStorage немає мови, визначаємо за IP
     const detectLanguage = async () => {
       try {
         const response = await fetch("https://ipapi.co/json/");
@@ -85,14 +35,11 @@ const LanguageRedirect = () => {
         const detectedLocale = COUNTRY_TO_LOCALE[country] || "en";
 
         localStorage.setItem("language", detectedLocale);
-
-        // Перенаправляємо тільки якщо виявлена мова відрізняється від поточної
         if (detectedLocale !== currentPathLocale) {
           window.location.href = `/${detectedLocale}`;
         }
       } catch (error) {
         console.warn("Помилка геолокації:", error);
-        // Встановлюємо англійську як мову за замовчуванням у разі помилки
         localStorage.setItem("language", "en");
       }
     };
